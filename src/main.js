ad0ce5be271fe6be60ac250997c756fb1601d801
ramlopez <83774204+ramlopez@users.noreply.github.com>
2023-06-11 14:44:05 -0300

    Remove Vue sample HelloWorld content

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// We need to call this to mount the App.vue root component to the <div> with id
+// "app" in index.html
 createApp(App).mount('#app')
